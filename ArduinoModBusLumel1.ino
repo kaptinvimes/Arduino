ab15a05881d7d4bb689ab04287d5cc96d9840bea
kaptinvimes <85481150+kaptinvimes@users.noreply.github.com>
2023-08-31 14:13:18 +0100

    Add files via upload

---
+++ ArduinoModBusLumel1.ino
@@ -0,0 +1,88 @@
+
+
+// Include these libraries for using the RS-485 and Modbus functions
+#include <RS485.h>
+#include <ModbusMaster485.h>
+#include <SPI.h>
+
+// Instantiate ModbusMaster object as slave ID 1
+ModbusMaster485 node(1);
+
+// Define one address for reading
+#define address 7505  // current display
+// Define the number of bytes to read
+#define bytesQty 2
+
+void setup()
+{
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW);  
+  
+byte array[4] = {0,0,0,0};
+  // Power on the USB for viewing data in the serial monitor
+  Serial.begin(9600);
+  delay(100);
+  // Initialize Modbus communication baud rate
+  node.begin(9600);
+
+  // Print hello message
+  Serial.println("Modbus communication over RS-485");
+  delay(100);
+  
+}
+
+
+void loop()
+{
+char display[9];
+char tempAr1[3];
+char tempAr2[3];
+char tempAr3[3];
+char tempAr4[3];
+String hex1;
+String hex2;
+String hex3;
+String hex4;
+int result =  node.readHoldingRegisters(address, bytesQty); 
+if (result != 0) { Serial.println("Communication error");   
+delay(1000); } 
+else {
+String current=String(node.getResponseBuffer(0),HEX);    // first 16 bits of 32 bit register in Hex
+String current1=String(node.getResponseBuffer(1),HEX);   // last 16 bits of 32 bit register in Hex
+String currentDisplayValue=current+current1;             // reconstruct 32 bit register in Hex
+hex1=currentDisplayValue.substring(0, 2); // will be byte 1
+hex2=currentDisplayValue.substring(2, 4); // will be byte 2 
+hex3=currentDisplayValue.substring(4, 6); // will be byte 3
+hex4=currentDisplayValue.substring(6, 8); // will be byte 4 
+hex1.toCharArray(tempAr1,3); // convert to character array
+hex2.toCharArray(tempAr2,3); // convert to character array
+hex3.toCharArray(tempAr3,3); // convert to character array
+hex4.toCharArray(tempAr4,3); // convert to character array
+int f1=strtol(tempAr1,NULL,16); // convert to decimal int
+int f2=strtol(tempAr2,NULL,16); // convert to decimal int
+int f3=strtol(tempAr3,NULL,16); // convert to decimal int
+int f4=strtol(tempAr4,NULL,16); // convert to decimal int
+byte array[4] = {f1,f2,f3,f4};
+Serial.print(convert(array),4);
+Serial.print("\n");
+delay(1000);    
+  }
+  node.clearResponseBuffer();
+}
+
+
+
+
+typedef union {
+ byte array[4];
+ float value;
+} ByteToFloat;
+
+float convert(byte* data) {
+ ByteToFloat converter;
+ for (byte i = 0; i < 4; i++){
+   converter.array[3-i] = data[i]; //or converter.array[i] = data[i]; depending on endianness 
+ }
+ return converter.value;
+}
+
---
+++ YalerWebService2.ino
@@ -0,0 +1,90 @@
+// Copyright (c) 2014, Yaler GmbH, Switzerland
+// All rights reserved
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <YalerEthernetServer.h>
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+//String dataTemp;
+String dataFinal;
+//char y;
+
+
+// Enter a MAC address for your controller below.
+// Some Ethernet shields have a MAC address printed on a sticker
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x42, 0x63 };
+
+// Local EthernetServer at http://LOCAL_IP/ (e.g. http://192.168.0.7/)
+//EthernetServer server(80);
+
+YalerEthernetServer server("try.yaler.net", 80, "gsiot-sw4f-0884");
+
+void setup() {
+  Serial.begin(9600);
+   // reserve 200 bytes for the inputString:
+  inputString.reserve(200); 
+  
+  Serial.println("Aquiring IP address...");
+  
+  if (Ethernet.begin(mac) == 0) 
+    {
+       Serial.println("DHCP failed."); 
+    } 
+  else 
+    {
+       Serial.println(Ethernet.localIP());
+       server.begin(); 
+    }
+
+}
+
+
+void loop() {
+EthernetClient client = server.available();    
+  // print the string when a newline arrives:
+  
+  if (stringComplete) 
+    {
+      Serial.print(inputString); 
+      // clear the string:
+      dataFinal=inputString;
+      inputString = "";
+      stringComplete = false;  
+    }
+
+   if (client && client.connected()) {
+    client.find("\r\n\r\n"); // Consume incoming request
+    sendResponse(client);
+    delay(100); // Give the Web browser time to receive the data
+   client.stop();
+  }
+
+}  // loop end brace
+
+ 
+ 
+void sendResponse(EthernetClient client) {
+  
+ client.print(dataFinal);  
+ dataFinal="";
+}  
+
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    if (inChar == byte(0x0a)) {
+      stringComplete = true;
+    } 
+  }
+}
+
+
+
+
+
